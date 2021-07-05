6e43baabc269137f3aa29472a2c7c1893c429fbf
atpap <atpap@LAPTOP-5A0IHL9B>
2021-07-05 11:41:11 +0300

    api controller and correct data format

--- CloudOnWebApp/Entities/Product.cs
+++ CloudOnWebApp/Entities/Product.cs
@@ -9,11 +9,11 @@ namespace CloudOnWebApp.Entities
     {
         public int Id { get; set; }
 
-        public int ExternalId { get; set; }
-        public int Code { get; set; }
+        public long ExternalId { get; set; }
+        public long Code { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
-        public int Barcode { get; set; }
+        public long Barcode { get; set; }
         public double RetailPrice { get; set; }
         public double WholePrice { get; set; }
         public double Discount { get; set; }
--- CloudOnWebApp/Options/CreateProductOptions.cs
+++ CloudOnWebApp/Options/CreateProductOptions.cs
@@ -3,11 +3,11 @@ namespace CloudOnWebApp.Options
 {
     public class CreateProductOptions
     {
-        public int ExternalId { get; set; }
-        public int Code { get; set; }
+        public long ExternalId { get; set; }
+        public long Code { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
-        public int Barcode { get; set; }
+        public long Barcode { get; set; }
         public double RetailPrice { get; set; }
         public double WholePrice { get; set; }
         public double Discount { get; set; }
